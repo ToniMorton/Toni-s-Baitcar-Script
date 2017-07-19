fc956754cf501d7a68347b2ee52c2b393e577a65
Toni Morton <haxor4evr@hotmail.com>
2017-07-19 08:59:35 -0700

    Add files via upload

---
+++ Baitcar/__resource.lua
@@ -0,0 +1,8 @@
+-- Client Scripts
+client_script 'client.lua'
+-- Server Scripts
+server_script 'server.lua'
+
+-----------------------------------------------------------------------------------------------------------------------
+-- CREATED BY TONI MORTON FOR THE FIVEM COMMUNITY, PLEASE GIVE CREDITS TO ME IF YOU USE THIS SCRIPT IN YOUR SERVER.  --
+-----------------------------------------------------------------------------------------------------------------------
